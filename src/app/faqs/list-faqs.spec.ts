336c8de5451a9927d43bb9ac45103c449e97c347
Rajkumar Patel <roysimpy@gmail.com>
2019-10-29 10:10:56 +0000

    responsive done

---
+++ src/app/faqs/list-faqs.spec.ts
@@ -0,0 +1,9 @@
+import { ListFaqsComponent } from './list-faqs.component';
+
+ describe('ListFaqsComponent',() => {
+   it('Should Initiate ',() => {
+     const result = new ListFaqsComponent(-1);
+     expect(result).toBe(1);
+
+   })
+ })
